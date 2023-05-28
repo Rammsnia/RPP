297dd7579240bf047e57894b6d0009225cb775bc
Rammsnia <kirill.starostin@internet.ru>
2023-05-28 15:43:58 +0400

    1
    1

--- Code RPP/LabaOdin/main.py
+++ Code RPP/LabaOdin/main.py
@@ -1,35 +1,56 @@
 import random
 
+# быстрая сортировка списка
+
 
 def quick_sort(arr):
     if len(arr) <= 1:
         return arr
+    else:
+        pivot = arr[0]
+        less = [x for x in arr[1:] if x <= pivot]
+        greater = [x for x in arr[1:] if x > pivot]
+        return quick_sort(less) + [pivot] + quick_sort(greater)
 
-    element = arr[0]
-    left = list(filter(lambda x: x < element, arr))
-    right = list(filter(lambda x: x > element, arr))
-    pivot = [i for i in arr if i == element]
 
-    return quick_sort(left) + pivot + quick_sort(right)
+# ручной ввод списка
 
-# проверка
-# создание списка от 0 до 20
 
-range_num = int(input("Введите количество чисел в рандомном массиве: "))
-arr = [i for i in range(0, range_num)]
-# перемешивание списка
-random.shuffle(arr)
-# вывод  перемешанного списка
-print("Перемешанный список: ")
-print(arr)
-print("Отсортированный список: ")
-print(quick_sort(arr))
-print("Введите последовательность (самописная реализация): ")
-# создание рандомного листа
-random_list_of_nums = [int(x) for x in input().split()]
-quick_sort(random_list_of_nums)
-print(random_list_of_nums)
-print("Введите последовательность (библиотечная функция): ")
-list_nums = [int(x) for x in input().split()]
-list_nums.sort()
-print(list_nums)
+def input_list():
+
+    try:
+        n = int(input("Введите количество элементов списка: "))
+        arr = [int(input("Введите элемент списка: ")) for i in range(n)]
+        return arr
+    except ValueError:
+        print("Ошибка: некорректный ввод элементов списка.")
+
+# автоматическая генерация списка
+
+
+def generate_list():
+    try:
+        n = int(input("Введите количество элементов списка: "))
+        arr = [random.randint(0, 100) for i in range(n)]
+        return arr
+    except ValueError:
+        print("Ошибка: некорректный ввод количества элементов списка.")
+
+
+if __name__ == '__main__':
+    while True:
+        choice = input("Введите '1', чтобы ввести элементы списка вручную, или '2', чтобы сгенерировать список автоматически: ")
+        if choice == '1':
+            arr = input_list()
+            break
+        elif choice == '2':
+            arr = generate_list()
+            break
+        else:
+            print("Ошибка: некорректный выбор.")
+    unsorted_arr = arr
+    print("Неотсортированный список: ", arr)
+    sorted_arr = quick_sort(arr)
+    print("Отсортированный список: ", sorted_arr)
+    arr.sort()
+    print("Отсортированный список библиотечной функцией: ", arr)
