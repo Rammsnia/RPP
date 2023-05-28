ee815219c29de2bfac43cc1306be872077b34470
Rammsnia <kirill.starostin@internet.ru>
2023-05-28 19:50:38 +0400

    1
    2

--- Code RPP/LabaDva/main.py
+++ Code RPP/LabaDva/main.py
@@ -1,26 +1,28 @@
 import numpy as np
 
-def generate_matrix(N, M):
-    """
-    Генерирует матрицу случайных нулей и единиц размера N на M
-    """
+
+# Генерирует матрицу случайных нулей и единиц размера N на M
+
+
+def generate_matrix(n, m):
     return np.random.randint(2, size=(N, M))
 
+# Добавляет к матрице еще один столбец, каждый элемент которого делает количество единиц в каждой строке чётным.
+
+
 def add_even_column(matrix):
-    """
-    Добавляет к матрице еще один столбец, каждый элемент которого делает количество единиц в каждой строке чётным.
-    """
     even_count = np.sum(matrix, axis=1) % 2 == 1
     return np.column_stack((matrix, even_count))
 
+# Сохраняет матрицу в файл.
+
+
 def save_matrix(matrix, file_name):
-    """
-    Сохраняет матрицу в файл.
-    """
-    with open(file_name, "w") as f:
         np.savetxt(f, matrix, fmt="%d")
 
 # Генерируем матрицу
+
+
 N = int(input("Введите высоту: "))
 M = int(input("Введите ширину матрицы: "))
 matrix = generate_matrix(N, M)
@@ -33,6 +35,8 @@ print("Матрица с дополнительным столбцом:")
 print(matrix_with_even_column)
 
 # Сохраняем матрицу в файл
-file_name = "matrix.txt"
+file_name_two = "matrix_first.txt"
+file_name = "matrix_result.txt"
+save_matrix(matrix, file_name_two)
 save_matrix(matrix_with_even_column, file_name)
 print(f"Результат сохранен в файл {file_name}.")
